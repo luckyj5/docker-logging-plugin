78c372a3fa82fe30b5c5402488c4e118bfedcd5c
Shubham Jain <luckyj5@users.noreply.github.com>
2018-03-12 14:51:08 -0700

    Added pytests

--- script.sh
+++ script.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 export SHELL=/bin/bash
-cd /go/docker-logging-plugin
+cd /go
+git clone https://github.com/luckyj5/docker-logging-plugin.git
+cd docker-logging-plugin
 git pull
-git checkout codeship
-go get -t -v ./...
-make
-sleep 5
+git checkout master
 
+#Go dependencies
+go get -t -v ./...
+
+#Compile
+make
+
+#Unit tests
+go test
+sleep 5
 
 ./plugin/rootfs/bin/splunk-log-plugin &
 nodejs run.js
 
-echo "Done with testing"
+cd test
+pip install -r requirements.txt
 
+#python -m pytest --splunkd-url https://52.53.254.149:8089 --splunk-user admin --splunk-password notchangeme --splunk-hec-url https://52.53.254.149:8088 --splunk-hec-token 7190F984-2C46-434F-8F3F-0455BD6B58A2 --docker-plugin-path /go/docker-logging-plugin/plugin/rootfs/bin/splunk-log-plugin --fifo-path /go/docker-logging-plugin/pipe
+echo "Running pytests...."
+python -m pytest --splunkd-url https://splunk-hec:8089 --splunk-user admin --splunk-password changeme --splunk-hec-url https://splunk-hec:8088 --splunk-hec-token 00000000-0000-0000-0000-000000000000 --docker-plugin-path /go/docker-logging-plugin/plugin/rootfs/bin/splunk-log-plugin --fifo-path /go/docker-logging-plugin/pipe
+
+echo "!-------------------Done with testing-------------------!"
+
+#Hack to send kill splunk container after testing.
 curl -u admin:changeme -k https://splunk-hec:8089/servicesNS/nobody/launcher/configs/conf-inputs -d 'name=batch:///Test-input' > /dev/null 2>&1
-
-tail -f /dev/null
