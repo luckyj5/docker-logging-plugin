c6ffd8f42bef9c4092f333eb3ee79d5d5bdce3f2
Shubham Jain <luckyj5@users.noreply.github.com>
2018-03-13 00:12:03 -0700

    added virt env to run pytest with python3.x

--- ci/script.sh
+++ ci/script.sh
@@ -23,9 +23,13 @@ echo "Running integration test..."
 nodejs ci/run.js
 
 cd test
+
+echo "Creating virtual env to run pytests..."
+pip3 install virtualenv
+virtualenv --python=python3.5 venv
+source venv/bin/activate
 pip install -r requirements.txt
 
-#python -m pytest --splunkd-url https://52.53.254.149:8089 --splunk-user admin --splunk-password notchangeme --splunk-hec-url https://52.53.254.149:8088 --splunk-hec-token 7190F984-2C46-434F-8F3F-0455BD6B58A2 --docker-plugin-path /go/docker-logging-plugin/plugin/rootfs/bin/splunk-log-plugin --fifo-path /go/docker-logging-plugin/pipe
 echo "Running pytests...."
 python -m pytest --splunkd-url https://splunk-hec:8089 --splunk-user admin --splunk-password changeme --splunk-hec-url https://splunk-hec:8088 --splunk-hec-token 00000000-0000-0000-0000-000000000000 --docker-plugin-path /go/docker-logging-plugin/plugin/rootfs/bin/splunk-log-plugin --fifo-path /go/docker-logging-plugin/pipe
 
