cf37c68aefe1c98e03304f9c30a6df44aead4812
Marko Locher <marko@locher.at>
2018-04-13 17:25:15 +0200

    Remove remaining files that were migrated to the `codeship` folder

--- ci/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd /go/docker-logging-plugin
-echo "Building docker logging plugin binary"
-make
--- ci/run_unit_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd /go
-go get -t -v ./...
-
-cd /go/docker-logging-plugin
-
-go test
